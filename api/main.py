c425cd610865e15748da16d4fb75cc3f615ee6fd
Mohammad Arshad <mdarshad1000@gmail.com>
2023-01-14 18:19:43 +0530

    Add chat feature

--- api/main.py
+++ api/main.py
@@ -1,30 +1,41 @@
 # Importing dependencies
-from flask import Flask, request, jsonify
+from flask import Flask, request
 from flask_cors import CORS, cross_origin
-
-import pdfplumber as pp
-import glob
-import os 
-
-from app import convert_pdf
+from app import convert_pdf, conversation
 
 app = Flask(__name__)
 CORS(app,)
 
+# Global var to store document
+source_document = None
 
 @cross_origin('*')
-@app.route('/explain', methods=["POST"])
+@app.route('/', methods=["POST"])
 def home():
 
-    list_of_files = glob.glob('/Users/arshad/Desktop/Projects/Legal/api/static/uploaded_pdfs/*.pdf') # * means all if need specific format then *.csv
-    latest_file = max(list_of_files, key=os.path.getctime) # selecting the latest file
-    print(latest_file)
+    # pdf upload path
+    path = "/Users/arshad/Desktop/Projects/Legal/backend/pdfs/"
 
-    document = convert_pdf(latest_file)
+    # fetch filename from Node backend
+    query = request.json["fileName"]
+
+    # convert pdf to text
+    global source_document
+    source_document = convert_pdf(path+query)
+
+    return "PDF RECEIVED"
 
 
-    return jsonify(document=document)
+@app.route('/chat', methods=['POST'])
+def chat():
+    
+    # fetch question
+    question = request.json["question"]
 
+    # generate answers
+    answer = conversation(source_document, question)
+
+    return {"Answer": answer}
 
 
 if __name__ == '__main__':
